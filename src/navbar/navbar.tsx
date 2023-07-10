970e27c88d71f4b305cfe6b1bb167f5cfe51daf0
Panth Patel <32ppatel@gmail.com>
2023-07-10 10:21:01 +0000

    Logout

--- src/navbar/navbar.tsx
+++ src/navbar/navbar.tsx
@@ -10,79 +10,119 @@ import { AppState } from "../redux/state";
 import { auth } from "../firebase/firebase";
 
 interface NavbarProps {
-    state: AppState
-    logOut: () => {}
+  state: AppState;
+  logOut: () => {};
 }
 
 export default function Navbar(props: NavbarProps) {
-    const [menuOn, setMenuOn] = useState(false);
-    const [index, setIndex] = useState(0);
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const location = useLocation();
-    const navigate = useNavigate();
+  const [menuOn, setMenuOn] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        window.addEventListener("resize", () => {
-            setWindowWidth(window.innerWidth);
-        });
-    }, []);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
 
-    function toggleMenuOn() {
-        setMenuOn(!menuOn);
-    }
+  function toggleMenuOn() {
+    setMenuOn(!menuOn);
+  }
 
-    return (
-        <div id="page">
-            <div id="navbar-outer">
-                <div id="navbar-content">
-                    <img src="/assets/logo.svg" alt=""/>
-                    <h2 style={{fontSize: windowWidth < 500 ? "1em" : "1.5em"}}>
-                        Employee Management
-                    </h2>
-                    {location.pathname !== "/" ? (
-                        <IconButton
-                            title={!menuOn ? "Show menu" : "Hide menu"}
-                            onClick={toggleMenuOn}
-                            style={{color: "white"}}
-                        >
-                            <ToggleIcon on={!menuOn} onIcon={<Menu/>} offIcon={<Clear/>}/>
-                        </IconButton>
-                    ) : (
-                        <div></div>
-                    )}
-                </div>
-                {
-                    menuOn && location.pathname !== "/" ? <div id="navbar-menu">
-                        <NavbarOption onClick={() => {
-                            setIndex(1);
-                            navigate("/home");
-                        }} title="Home" selected={index === 1}/>
-                        <NavbarOption onClick={() => {
-                            setIndex(2);
-                            navigate("/profile");
-                        }} title="Profile" selected={index === 2}/>
-                        <NavbarOption onClick={() => {
-                            setIndex(3);
-                            navigate("/notifications");
-                        }} title="Notifications" selected={index === 3}/>
-                        <NavbarOption onClick={() => {
-                            setIndex(4);
-                            navigate("/chat");
-                        }} title="Chat with others" selected={index === 4}/>
-                        {props.state.user!.isAdmin ? <NavbarOption onClick={() => {
-                            setIndex(5);
-                            navigate("/report-page");
-                        }} title="Report Page" selected={index === 5}/> : <></>}
-                        {props.state.user !== null ? <NavbarOption onClick={async () => {
-                            setIndex(6);
-                            props.logOut();
-                            await auth.signOut();
-                            navigate("/");
-                        }} title="Log out" selected={false}/> : <></>}
-                    </div> : <></>
-                }
-            </div>
-            <Outlet/>
+  return (
+    <div id="page">
+      <div id="navbar-outer">
+        <div id="navbar-content">
+          <img src="/assets/logo.svg" alt="" />
+          <h2 style={{ fontSize: windowWidth < 500 ? "1em" : "1.5em" }}>
+            Employee Management
+          </h2>
+          {location.pathname !== "/" ? (
+            <IconButton
+              title={!menuOn ? "Show menu" : "Hide menu"}
+              onClick={toggleMenuOn}
+              style={{ color: "white" }}
+            >
+              <ToggleIcon on={!menuOn} onIcon={<Menu />} offIcon={<Clear />} />
+            </IconButton>
+          ) : (
+            <div></div>
+          )}
         </div>
-    );
+        {menuOn && location.pathname !== "/" ? (
+          <div id="navbar-menu">
+            <NavbarOption
+              onClick={() => {
+                setIndex(1);
+                setMenuOn(false);
+                navigate("/home");
+              }}
+              title="Home"
+              selected={index === 1}
+            />
+            <NavbarOption
+              onClick={() => {
+                setIndex(2);
+                setMenuOn(false);
+                navigate("/profile");
+              }}
+              title="Profile"
+              selected={index === 2}
+            />
+            <NavbarOption
+              onClick={() => {
+                setIndex(3);
+                setMenuOn(false);
+                navigate("/notifications");
+              }}
+              title="Notifications"
+              selected={index === 3}
+            />
+            <NavbarOption
+              onClick={() => {
+                setIndex(4);
+                setMenuOn(false);
+                navigate("/chat");
+              }}
+              title="Chat with others"
+              selected={index === 4}
+            />
+            {props.state.user?.isAdmin ?? false ? (
+              <NavbarOption
+                onClick={() => {
+                  setIndex(5);
+                  setMenuOn(false);
+                  navigate("/report-page");
+                }}
+                title="Report Page"
+                selected={index === 5}
+              />
+            ) : (
+              <></>
+            )}
+            {props.state.user !== null ? (
+              <NavbarOption
+                onClick={async () => {
+                  setIndex(6);
+                  setMenuOn(false);
+                  props.logOut();
+                  await auth.signOut();
+                  navigate("/");
+                }}
+                title="Log out"
+                selected={false}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+      <Outlet />
+    </div>
+  );
 }
