3bfc9b891e76c5b0a73115f78379c688d2e31711
Panth Patel <32ppatel@gmail.com>
2023-07-12 09:32:26 +0000

    Reschedule employee shifts

--- src/employee-view/employee-view.tsx
+++ src/employee-view/employee-view.tsx
@@ -5,6 +5,7 @@ import { collection, onSnapshot, query, where, doc } from "firebase/firestore";
 import ClearIcon from "@mui/icons-material/Clear";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
 import IconButton from "@mui/material/IconButton";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { Employee } from "../home/home";
@@ -12,6 +13,10 @@ import dayjs from "dayjs";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import ReactModal from "react-modal";
+import "dayjs/locale/en-gb";
 
 interface EmployeeViewProps {
   employee: Employee;
@@ -29,6 +34,14 @@ export default function EmployeeView(props: EmployeeViewProps) {
   const [date, setDate] = useState(dayjs());
   const [time, setTime] = useState<"morning" | "evening">("morning");
   const [isAddingShift, setIsAddingShift] = useState(false);
+  const [isReschedulingShift, setIsReschedulingShift] = useState(false);
+  const [rescheduleDate, setRescheduleDate] = useState(dayjs());
+  const [rescheduleTime, setRescheduleTime] = useState<"morning" | "evening">(
+    "morning"
+  );
+  const [rescheduleShiftId, setRescheduleShiftId] = useState<string | null>(
+    null
+  );
 
   function normalise(a: number) {
     return a / Math.abs(a);
@@ -88,18 +101,23 @@ export default function EmployeeView(props: EmployeeViewProps) {
         <p>Add a shift</p>
       </div>
       {isAddingShift ? (
-        <div>
-          <DatePicker
-            label="Shift date"
-            value={date}
-            onChange={(d) => {
-              if (d) setDate(d);
-            }}
-          />
+        <div id="employee-add-shift-details">
+          <LocalizationProvider
+            dateAdapter={AdapterDayjs}
+            adapterLocale="en-gb"
+          >
+            <DatePicker
+              label="Shift date"
+              value={date}
+              onChange={(d) => {
+                if (d) setDate(d);
+              }}
+            />
+          </LocalizationProvider>
           <label htmlFor="employee-shift-add-time">Time: </label>
           <RadioGroup
             row
-            aria-labelledby="demo-radio-buttons-group-label"
+            id="employee-shift-add-time"
             defaultValue="morning"
             value={time}
             onChange={(e) => {
@@ -120,20 +138,18 @@ export default function EmployeeView(props: EmployeeViewProps) {
           <div
             id="employee-shift-add-submit"
             onClick={async () => {
-              await fetch(
-                `${apiURL}shifts/add?admin=${auth.currentUser!.uid}`,
-                {
-                  method: "PUT",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({
-                    employee: props.employee.uid,
-                    time: time,
-                    date: date,
-                  }),
-                }
-              );
+              await fetch(`${apiURL}shift/add?admin=${auth.currentUser!.uid}`, {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  employee: props.employee.uid,
+                  time: time,
+                  date: date.format("YYYY-MM-DD"),
+                }),
+              });
+              setIsAddingShift(false);
             }}
           >
             <AddIcon />
@@ -151,29 +167,119 @@ export default function EmployeeView(props: EmployeeViewProps) {
       >
         There are no shifts to display
       </p>
+      <ReactModal
+        ariaHideApp={false}
+        id="employee-reschedule-shift-modal"
+        isOpen={isReschedulingShift}
+        onRequestClose={() => {
+          setRescheduleShiftId(null);
+          setRescheduleDate(dayjs());
+          setRescheduleTime("morning");
+          setIsReschedulingShift(false);
+        }}
+        contentLabel="Reschedule Shift"
+      >
+        <div id="employee-reschedule-shift-details">
+          <h1>Reschedule Shift</h1>
+          <p style={{ marginBottom: "10px" }}>
+            (Click outside to discard changes)
+          </p>
+          <LocalizationProvider
+            dateAdapter={AdapterDayjs}
+            adapterLocale="en-gb"
+          >
+            <DatePicker
+              label="Shift date"
+              value={rescheduleDate}
+              onChange={(d) => {
+                if (d) setRescheduleDate(d);
+              }}
+            />
+          </LocalizationProvider>
+          <label htmlFor="employee-shift-reschedule-time">Time: </label>
+          <RadioGroup
+            row
+            id="employee-shift-reschedule-time"
+            defaultValue="morning"
+            value={rescheduleTime}
+            onChange={(e) => {
+              setRescheduleTime(e.target.value as "morning" | "evening");
+            }}
+          >
+            <FormControlLabel
+              value="morning"
+              control={<Radio />}
+              label="Morning"
+            />
+            <FormControlLabel
+              value="evening"
+              control={<Radio />}
+              label="Evening"
+            />
+          </RadioGroup>
+          <div
+            id="employee-shift-reschedule-submit"
+            onClick={async () => {
+              await fetch(
+                `${apiURL}shift/update/${rescheduleShiftId}?admin=${
+                  auth.currentUser!.uid
+                }`,
+                {
+                  method: "PATCH",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    employee: props.employee.uid,
+                    time: rescheduleTime,
+                    date: rescheduleDate.format("YYYY-MM-DD"),
+                  }),
+                }
+              );
+              setIsReschedulingShift(false);
+            }}
+          >
+            <EditIcon />
+            <p style={{ marginLeft: "5px" }}>Reschedule this shift</p>
+          </div>
+        </div>
+      </ReactModal>
       {shifts.map((e) => (
         <div className="shift" key={e.id}>
           <div>
             <h1>{e.date.split("-").reverse().join("/")}</h1>
             <p>{e.time.substring(0, 1).toUpperCase() + e.time.substring(1)}</p>
           </div>
-          <IconButton
-            onClick={async () => {
-              await fetch(
-                apiURL +
-                  "shift/delete/" +
-                  e.id +
-                  "?admin=" +
-                  auth.currentUser?.uid,
-                {
-                  method: "DELETE",
-                }
-              );
-            }}
-            title="Delete this shift"
-          >
-            <DeleteIcon />
-          </IconButton>
+          <div>
+            <IconButton
+              title="Reschedule shift"
+              onClick={() => {
+                setRescheduleShiftId(e.id);
+                setRescheduleDate(dayjs(e.date));
+                setRescheduleTime(e.time);
+                setIsReschedulingShift(true);
+              }}
+            >
+              <EditIcon />
+            </IconButton>
+            <IconButton
+              onClick={async () => {
+                await fetch(
+                  apiURL +
+                    "shift/delete/" +
+                    e.id +
+                    "?admin=" +
+                    auth.currentUser?.uid,
+                  {
+                    method: "DELETE",
+                  }
+                );
+              }}
+              title="Delete this shift"
+            >
+              <DeleteIcon />
+            </IconButton>
+          </div>
         </div>
       ))}
     </div>
