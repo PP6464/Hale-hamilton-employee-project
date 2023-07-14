c70c66d66a889afc2e47495a2935790d63bd8e10
Panth Patel <panthhp@outlook.com>
2023-07-14 21:42:44 +0100

    121 chat

--- src/chat/121/121.tsx
+++ src/chat/121/121.tsx
@@ -1,22 +1,110 @@
 import './121.css';
-import { useEffect, useState } from "react";
-import { useNavigate, Link, useParams } from "react-router-dom";
-import { auth, firestore } from "../../firebase/firebase";
-import { onSnapshot, query, orderBy, collection } from "firebase/firestore";
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {apiURL, auth, firestore} from "../../firebase/firebase";
+import {onSnapshot, query, orderBy, collection, doc, DocumentReference} from "firebase/firestore";
+import {User} from "../../redux/state";
+import SendIcon from "@mui/icons-material/Send";
+import IconButton from '@mui/material/IconButton';
+
+interface Message {
+    text: string;
+    from: DocumentReference;
+    to: DocumentReference;
+    timestamp: Date;
+    id: string;
+}
 
 export default function OneToOneChat() {
     const navigate = useNavigate();
-    const { uid } = useParams();
+    const {uid} = useParams();
+    const [userData, setUserData] = useState<User>({
+        email: "blank.email@circor.com",
+        isAdmin: false,
+        name: "Blank name",
+        photoURL: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+        uid: uid ?? "",
+    });
+    const [msgText, setMsgText] = useState("");
+    const [messages, setMessages] = useState<Message[]>([]);
 
+    useEffect(() => {
+        onSnapshot(
+            doc(firestore, `users/${uid}`),
+            (snapshot) => {
+                setUserData({
+                    ...(snapshot.data() as any),
+                });
+            },
+        );
+        onSnapshot(
+            query(collection(firestore, `121/${[auth.currentUser!.uid, uid].sort().join()}/messages`), orderBy('time')),
+            (snapshot) => {
+                setMessages(
+                    snapshot.docs.map((e) => {
+                        return {
+                            ...(e.data() as any),
+                            id: e.id,
+                            timestamp: e.data()['time'].toDate(),
+                        };
+                    })
+                );
+            },
+        );
+    }, [uid]);
     useEffect(() => {
         if (auth.currentUser === null) {
             navigate(`/?route=/chat/121/${uid}`)
         }
-    }, [navigate]);
+    }, [navigate, uid]);
 
     return (
-        <div>
-            <h1>121 Chat</h1>
+        <div className="container">
+            <div id="one-to-one-header">
+                <img src={userData.photoURL} alt=""/>
+                <div>
+                    <h1>{userData.name}</h1>
+                    <p>{userData.email}</p>
+                </div>
+            </div>
+            <p style={{display: messages.length !== 0 ? "none" : "block"}}>You have no messages to display</p>
+            <div id="one-to-one-messages">
+                {
+                    messages.map((e) => (
+                        <div key={e.id}>
+                            <div className="message" data-from-user={e.from.id !== uid} style={{width: "100vw", display: "flex", alignItems: "center", justifyContent: e.from.id !== uid ? "flex-end" : "flex-end"}}>
+                                <div>
+                                    <img src={e.from.id !== uid ? auth.currentUser!.photoURL! : userData.photoURL} alt=""/>
+                                    <div>
+                                        <h3>{e.from.id !== uid ? auth.currentUser!.displayName : userData.name}</h3>
+                                        <p>{e.timestamp.toISOString().split("T")[1].split(".")[0] + " " + e.timestamp.toISOString().split("T")[0].split("-").reverse().join("/")}</p>
+                                    </div>
+                                </div>
+                                <p>{e.text}</p>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div id="one-to-one-msg">
+                <input placeholder={`Send a message to ${userData.name}`} value={msgText} onChange={(e) => setMsgText(e.target.value)} />
+                <IconButton onClick={async () => {
+                    await fetch(`${apiURL}chat/message`, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            from: auth.currentUser!.uid,
+                            to: uid,
+                            text: msgText,
+                        }),
+                    });
+                    setMsgText("");
+                }} title={msgText === "" ? "Message text required to send" : `Send ${msgText} to ${userData.name}`}>
+                    <SendIcon />
+                </IconButton>
+            </div>
         </div>
-    );    
+    );
 }
