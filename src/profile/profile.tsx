e0a78c220a4ffba439f2c7a4c79c840ca9ed9129
PP6464 <panthhp@outlook.com>
2023-07-18 10:02:27 +0100

    reauthenticate with password to change profile

--- src/profile/profile.tsx
+++ src/profile/profile.tsx
@@ -10,7 +10,8 @@ import {
   listAll,
   deleteObject,
 } from "firebase/storage";
-import { updateProfile } from "firebase/auth";
+import ReactModal from "react-modal";
+import { updateProfile, reauthenticateWithCredential, EmailAuthProvider, updatePassword } from "firebase/auth";
 import { updateDoc, doc } from "firebase/firestore";
 
 interface ProfileProps {
@@ -22,11 +23,17 @@ export default function Profile(props: ProfileProps) {
   const navigate = useNavigate();
   const [isModifyingProfile, setIsModifyingProfile] = useState(false);
   const [name, setName] = useState(auth.currentUser?.displayName ?? "");
+  const [password, setPassword] = useState("");
   const [filePic, setFilePic] = useState<string | null>(null);
   const [photoURL, setPhotoURL] = useState(
     auth.currentUser?.photoURL ??
       "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
   );
+  const [reAuthDetails, setReAuthDetails] = useState({
+    email: auth.currentUser!.email!,
+    password: "",
+  });
+  const [reAuthRequired, setReAuthRequired] = useState(true);
 
   useEffect(() => {
     if (!auth.currentUser) {
@@ -40,6 +47,30 @@ export default function Profile(props: ProfileProps) {
 
   return (
     <div className="container">
+      <ReactModal
+        isOpen={reAuthRequired}
+        onRequestClose={() => {}}
+        ariaHideApp={false}
+        id="profile-reauth-modal">
+        <div id="profile-reauth-content">
+          <h1>Re-authentication required</h1>
+          <input value={reAuthDetails.email} onChange={(e) => setReAuthDetails({...reAuthDetails, email: e.target.value})} placeholder="Email" type="email"/>
+          <input value={reAuthDetails.password} onChange={(e) => setReAuthDetails({...reAuthDetails, password: e.target.value})} placeholder="Password" type="password"/>
+          <button onClick={async () => {
+            try {
+              await reauthenticateWithCredential(auth.currentUser!, EmailAuthProvider.credential(reAuthDetails.email, reAuthDetails.password));
+              setPassword(reAuthDetails.password);
+              setReAuthRequired(false);
+            } catch (e) {
+              if (e instanceof Error) {
+                alert(e.message);
+              }
+            }
+          }}>
+            <h3>Re-authenticate</h3>
+          </button>
+        </div>
+      </ReactModal>
       {isModifyingProfile ? (
         <div className="container" style={{ marginTop: "10px" }}>
           <img
@@ -53,6 +84,15 @@ export default function Profile(props: ProfileProps) {
             value={name}
             onChange={(e) => {
               setName(e.target.value);
+            }}
+          />
+          <input
+            id="profile-password"
+            placeholder="Enter your password"
+            value={password}
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
             }}
           />
           <p>Photo options:</p>
@@ -130,6 +170,7 @@ export default function Profile(props: ProfileProps) {
                 displayName: name,
                 photoURL: photoURL,
               });
+              if (password !== reAuthDetails.password) await updatePassword(auth.currentUser!, password);
               await auth.currentUser!.reload();
               await updateDoc(
                 doc(firestore, `users/${auth.currentUser!.uid}`),
@@ -156,7 +197,7 @@ export default function Profile(props: ProfileProps) {
               toggleIsModifyingProfile();
             }}
           >
-            <h1>Discard Changes</h1>
+            <h3>Discard Changes</h3>
           </button>
         </div>
       ) : (
@@ -168,6 +209,7 @@ export default function Profile(props: ProfileProps) {
               ? "Administrator"
               : "Employee"}
           </p>
+          <p>Department: {props.state.user?.department}</p>
           <button
             id="modify-profile-button"
             onClick={toggleIsModifyingProfile}
